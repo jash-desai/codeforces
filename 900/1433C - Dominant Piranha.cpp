411f3438d817d51b3d05855ef462b7f110feb4a1
= <jash.13.desai@gmail.com>
2022-08-12 16:33:54 +0530

    1433C + 1535B

--- 900/1433C - Dominant Piranha.cpp
+++ 900/1433C - Dominant Piranha.cpp
@@ -1,7 +1,7 @@
 /*
-Problem Name: Array Reodering
+Problem Name: Dominant Piranha
 Problem Type: A - 900
-Problem Link: https://codeforces.com/problemset/problem/1535/B
+Problem Link: https://codeforces.com/problemset/problem/1433/C
 Author: Jash Desai (jash13desai)
 */
 // ---------- HEADER ----------
@@ -13,33 +13,32 @@ void see(T&... args) { ((cin >> args), ...);}
 template<typename... T>
 void put(T&&... args) { ((cout << args << " "), ...);}
 template<typename... T>
-void putl(T&&... args) { ((cout << args << " "), ...); cout << '\n';}
+void putl(T&&... args) { ((cout << args << " "), ...); cout<<'\n';}
 // ---------- MACROS ----------
 #define int long long
 #define pb push_back
 #define mp make_pair
-#define ff first
-#define ss second
+#define fi first
+#define se second
 #define ll long long
 #define ld long double
 #define ull unsigned long long
-#define print(x) cout << (x)
+#define print(x) cout << x
 #define sz size()
-#define vc(x) vector<x>
+#define vc vector
 #define nl cout<<'\n'
 #define sqof(a) (a)*(a)
 #define vi vector<int>
-#define vvi vector<vector<int>>
 #define pii pair<int,int>
 #define vii vector<pii>
 #define IOS ios_base:: sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);cin.exceptions(cin.failbit)
 #define all(x) (x).begin(), (x).end()
 #define setpr(x) cout<<setprecision(x)<<fixed
-#define setbits(x) builtin_popcount((x))
 #define tostr(x) to_string(x)
-#define rep(i,a,b) for(int i=a; i<b; i++)
-#define seeArr(arr,n) for(int i=0;i<n;i++){cin>>arr[i];}
+#define loop(i,a,b) for(int i=a; i<b; i++)
+#define seeArr(arr,x,y) for(int i=x;i<y;i++){cin>>arr[i];}
 #define seeVi(v,n) for(int i=0;i<n;i++){int x; cin>>x; v.pb(x);}
+#define seeStr(s,n) for(int i=0;i<n;i++){int x; cin>>x; s.pb(x);}
 // ---------- DEBUG ----------
 #ifndef ONLINE_JUDGE
 #define debug(x) cerr << #x <<" : "; _print(x); cerr << '\n';
@@ -53,55 +52,57 @@ void _print(ld t) {cerr << t;}
 void _print(double t) {cerr << t;}
 void _print(ull t) {cerr << t;}
 void _print(bool t) {cerr << t;}
-void _print(pair<int, int> t) {cerr << "[ " << t.ff << ", " << t.ss << " ]"; nl;}
+void _print(pair<int,int> t) {cerr << "[ " << t.fi << ", " << t.se << " ]"; nl;}
 template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 // ---------- CONSTANTS ----------
 const ll infmax = INT_MAX;
 const ll infmin = INT_MIN;
-const ll N1 = 1e3+2;
-const ll N2 = 1e5+2;
 const ld ep = 0.0000001;
 const ld pi = acos(-1.0);
 const ll md = 1000000007;
-// ---------- GLOBALS ----------
-
 // ---------- FUNCTIONS ----------
-int gcd(int a, int b){return (!b) ? a : gcd(b, a % b);}
-int fact(int n){if(n==1 || n==0){return 1;} return (n*fact(n-1));}
+
 // ---------- SOLUTIONS ----------
 void solve() {
-    int n; see(n);
-    vi v(n); seeVi(v,n);
-    sort(v.begin(),v.end(),greater<int>());
-    rep(i,0,n){
-        if(v[i]&1){
-            v.pb(v[i]);
-            v[i]=0;
+	int n; see(n);
+    int a;  int m=0;
+    vi v; 
+    for(int i=0; i<n; i++){
+        cin>>a;
+        m = max(m,a);
+        v.pb(a);
+    }
+    int pos = 0;
+    for(int i=0; i<n; i++){
+        if( (v[i] > v[i-1] && v[i] == m) && (i > 0) ){
+            pos = i+1;
+            break;
+        }
+        else if( (v[i] > v[i+1] && v[i] == m) && (i < n-1) ){
+            pos = i+1;
+            break;
         }
     }
-    int cnt=0;
-    for(int i=0;i<v.size();i++){
-        if(v[i]==0) continue;
-        for(int j=i+1;j<v.size();j++){
-            if(v[j]==0) continue;
-            if(gcd(v[i],(2*v[j]))>1) cnt++;
-        }
+    if(pos > 0){
+        cout<<pos;
     }
-    cout<<cnt;
+    else{
+        cout<<-1;
+    }
 }
 // ---------- MAIN ----------
-int32_t main() {
+signed main() {
 #ifndef ONLINE_JUDGE
     freopen("error.txt", "w", stderr);
 #endif
     IOS;
-    int t = 1;
+    int t=1;
     cin>>t;
-    while (t--) {
+    while(t--){
         solve();
         nl;
     }
     return 0;
 }
-// ---------- TEMP ----------
+// ---------- ROUGHCODE ----------
 // ---------- END ----------
